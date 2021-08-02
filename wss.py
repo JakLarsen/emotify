89c6e2dc8b6dfbc8131495be9ced14cbbaf966b4
= <jakelarsendk@gmail.com>
2021-08-01 20:17:21 -0500

    connected to device but no data stream yet

--- wss.py
+++ wss.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python
-
-# WSS (WS over TLS) client example, with a self-signed certificate
-
 import asyncio
 import pathlib
 import ssl
 import websockets
 import json
 
+
+#Setup a websocket connection to the Cortex API from Emotiv
 async def hello():
 
     uri = "wss://localhost:6868"
@@ -16,10 +14,9 @@ async def hello():
         uri, ssl=True
     ) as websocket:
 
+        #checking connection
         greeting = '{"id":1,"jsonrpc":"2.0","method":"getCortexInfo"}'
-        
         await websocket.send(greeting)
-        print(f"> {greeting}")
         response = await websocket.recv()
         print(f"< {response}")
 
@@ -34,8 +31,11 @@ async def hello():
         print(f"< {response_auth}")
 
 
+
+        #TOKEN
         token = json.loads(response_auth)['result']['cortexToken']
         print(token)
+
 
 
         user_info_request = '{"id":1,"jsonrpc":"2.0","method":"getUserInformation", "params": {"cortexToken":' + f'"{token}"' + '}}'
@@ -51,14 +51,36 @@ async def hello():
         query_headset = '{"id":1,"jsonrpc":"2.0","method":"queryHeadsets", "params": {"id":"INSIGHT-A2D203D1"}}'
         await websocket.send(query_headset)
         response_query = await websocket.recv()
-        print(response_query)
-
+        print(f"<{response_query}")
 
         session_request = '{"id":1,"jsonrpc":"2.0","method":"createSession", "params": {"cortexToken":' + f'"{token}",' + '"headset":"INSIGHT-A2D203D1","status":"active"}}'
         await websocket.send(session_request)
         response_open_session = await websocket.recv()
-        print(response_open_session)
+        print(f"<{response_open_session}")
 
+        current_session = '{"id":1,"jsonrpc":"2.0","method":"querySessions", "params": {"cortexToken":' + f'"{token}"' + '}}'
+        await websocket.send(current_session)
+        response_current_session = await websocket.recv()
+
+
+
+        #Session ID
+        session_id = json.loads(response_current_session)['result'][0]['id']
+        print(f"<Session_id: ******* {session_id}")
+
+
+
+        stream = '{"id":1,"jsonrpc":"2.0","method":"subscribe", "params": {"cortexToken":' + f'"{token}"' + '"session":' + f'"{session_id}",' + '"streams":["mot"]}}'
+        await websocket.send(stream)
+        response_stream = await websocket.recv()
+        # {"error":{"code":-32700,"data":{"behavior":"unterminated object at offset 404"},"message":"Parse Error."},"id":null,"jsonrpc":"2.0"}
+        # print(response_stream)
+
+
+        # unstream = '{"id":1,"jsonrpc":"2.0","method":"unsubscribe", "params": {"cortexToken":' + f'"{token}"' + '"session":' + f'"{session_id}",' + '"streams":["mot"]}}'
+        # await websocket.send(unstream)
+        # response_unstream = await websocket.recv()
+        # print(f"<Subscription: ******* {response_unstream}")
 
 
 # "INSIGHT-A2D203D1"
