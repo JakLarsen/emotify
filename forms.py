a5ac36101fabaef1194ed1e646731e5577e5c777
= <jakelarsendk@gmail.com>
2021-08-22 10:12:41 -0500

    signup.css v1.0 almost finish

--- forms.py
+++ forms.py
@@ -6,10 +6,9 @@ from wtforms.validators import DataRequired, Length #need to add email validator
 class UserAddForm(FlaskForm):
     """ Form for adding a new User"""
 
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('E-mail', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
-    image_url = StringField('(Optional) Image URL')
+    username = StringField('Username', validators=[DataRequired()], render_kw={"placeholder": "Enter a username."})
+    email = StringField("What's your e-mail", validators=[DataRequired()], render_kw={"placeholder": "Enter your email."})
+    password = PasswordField('Create a password', validators=[DataRequired(), Length(min=6)], render_kw={"placeholder": "Create a password."})
 
 class LoginForm(FlaskForm):
     """Form for logging in an existing User"""
