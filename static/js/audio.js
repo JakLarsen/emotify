195b3a305d7cbbebaf12aeb7e5a7e17d06cfd2dc
= <jakelarsendk@gmail.com>
2021-08-30 09:56:27 -0500

    updated logic and helper functions for basic playing from library.html. Need to update currSong div next

--- static/js/audio.js
+++ static/js/audio.js
@@ -1,28 +1,74 @@
+
+
+
+                    //GLOBALS
+
+
+
 let songDiv = document.getElementById('lib-bot')
-
 let isPlaying = true
+let currSong = null
 
 
 
+                    //GENERAL HELPER FUNCTIONS
 
-function play(songId) {
-    let audio = document.getElementById(songId)
 
-    if (isPlaying==true){
-        isPlaying=false
-        audio.volume = 0.2;
-        audio.play();
-    }
-    else{
-        isPlaying=true
-        console.log(audio.currentTime)
-        audio.pause()
-    }
-   
+
+function endsWithNumber(str){
+    return !isNaN(str.slice(-1))
+}
+function convertLastCharToInt(str){
+    return parseInt(str.slice(-1))
 }
 
+
+
+                    //AUDIO PLAYER HELPERS
+
+
+
+function play(songID) {
+    let audio = document.getElementById(songID)
+
+    audio.volume = 0.2;
+    audio.play();   
+}
+
+function pause(songID){
+    let audio = document.getElementById(songID)
+
+    audio.pause();   
+}
+
+
+
+                    //AUDIO PLAYER HANDLERS
+
+function changeCurrSongDiv(songDivID){
+
+}
+
+
+                   
+//Takes target div id, validates it, and plays associated song
 songDiv.addEventListener("click", function(evt){
-    let songId = `audio_${evt.path[0].id}`
-    console.log(songId)
-    play(songId)    
+
+    let songDivID = evt.path[0].id
+    let songIDTarget = `audio_${songDivID}`
+    let isProperSongID = endsWithNumber(songIDTarget)
+    
+    if (isProperSongID && !isPlaying){
+        isPlaying = true
+        currSong = songDivID
+        if (songDivID != currSong){
+            changeCurrSongDiv(songDivID)
+        }
+        play(songIDTarget)  
+    }
+    else{
+        isPlaying = false
+        pause(songIDTarget) 
+        console.log('not an integer id')
+    }  
 })
