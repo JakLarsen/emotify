9c7a834f88b6fe6d8e97ff67202abd95e91fb317
= <jakelarsendk@gmail.com>
2021-09-03 18:02:22 -0500

    Next - Focus on populating previous and next songs then get the swap working with the headset

--- static/js/app.js
+++ static/js/app.js
@@ -25,3 +25,13 @@ $('#tl-playlist-wrap').click(function(){
     console.log('Your Playlists Button clicked and attempt to load midCon')
     midCon.load("/your-playlists")
 });
+$('#icon-title-link').click(function(){
+    console.log('You clicked the logo')
+    midCon.load("/home")
+});
+$('#ml-create-playlist-form').click(function(){
+    console.log('You clicked the logo')
+    midCon.load("/create-playlist")
+});
+
+
