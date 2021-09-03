de6e27002ca879caf570ba245d205ae9659c6d60
= <jakelarsendk@gmail.com>
2021-09-03 13:24:04 -0500

    library.js; initial loaded songs; js formatting

--- static/js/audio.js
+++ static/js/audio.js
@@ -18,9 +18,11 @@ let currSong = null
 
 
 function endsWithNumber(str){
+    console.log('endsWithNumber() called')
     return !isNaN(str.slice(-1))
 }
 function convertLastCharToInt(str){
+    console.log('convertLastCharToInt() called')
     return parseInt(str.slice(-1))
 }
 
@@ -28,8 +30,10 @@ function convertLastCharToInt(str){
 
                     //AUDIO PLAYER HELPERS
 
+        
 
 function updateIsPlaying(){
+    console.log('updateIsPlaying called')
     if(isPlaying == true){
         isPlaying = false
         console.log(`isPlaying is now: ${isPlaying}`)
@@ -41,30 +45,32 @@ function updateIsPlaying(){
 }
 //UPDATE CURRENT SONG TARGET
 function updateCurrSong(songIDTarget){
+    console.log('updateCurrSong called')
     currSong = songIDTarget
 }
 function play(songIDTarget) {
+    console.log('play() called')
     let audio = document.getElementById(songIDTarget)
     console.log(`Trying to play song: ${audio}`)
     audio.volume = 0.2;
     audio.play();   
 }
-
 function pause(songIDTarget){
+    console.log('pause() called')
     let audio = document.getElementById(songIDTarget)
     console.log(`Trying to pause song: ${audio}`)
     audio.pause();   
 }
 function stop(currSongIDTarget){
+    console.log('stop() called')
     let audio = document.getElementById(currSongIDTarget)
     console.log(`Trying to stop song: ${audio}`)
     audio.pause();
     audio.currentTime = 0;
-    
 }
 //UPDATE CURRENT SONG IN AUDIO APP
 function changeCurrSongDiv(entireSongDiv){
-    console.debug('changeCurrSongDiv called')
+    console.log('changeCurrSongDiv called')
 
     let currTitle = document.getElementById('curr-playing-song-title')
     let currArtist = document.getElementById('curr-playing-song-artist')
@@ -88,15 +94,19 @@ function changeCurrSongDiv(entireSongDiv){
                     //AUDIO PLAYER HANDLERS
 
 
+
 function songValidated(songIDTarget){
+    console.log('songValidated() called')
     return endsWithNumber(songIDTarget)
 }
 
 function isCurrSong(songIDTarget){
+    console.log('isCurrSong() called')
     return songIDTarget == currSong
 }
 
 function audioHandler(songIDTarget){
+    console.log('audioHandler() called')
     if(songValidated(songIDTarget)){
         if(isCurrSong(songIDTarget)){
             if(isPlaying){
@@ -120,63 +130,16 @@ function audioHandler(songIDTarget){
         console.log('Not a proper song Div ID')
     }  
 }
-      
-
-
-//MAIN LIBRARY AUDIO HANDLER
-//
-songDiv.addEventListener("click", function(evt){
-
-    let entireSongDiv = evt.path[1]
-    let songDivID = evt.path[0].id
-    //this is the ID we want for document.getEle... to use audio. commands on
-    let songIDTarget = `audio_${songDivID}`
-    let isProperSongID = endsWithNumber(songIDTarget)
-    
-    //Did we target a proper songDiv?       
-    if (isProperSongID){
-        console.log('We clicked a proper song ID')
-        //Is it the previously targetted songDiv that is already playing or paused?
-        //-Then pause/play it and update isPlaying
-        if (songIDTarget == currSong){
-            console.log(`Our target: ${songIDTarget} is the same as current song: ${currSong}`)
-            if (isPlaying){
-                pause(songIDTarget)
-            }
-            else{
-                play(songIDTarget)
-            }
-            updateIsPlaying()
-        }   
-        //Is it another song we are trying to play?
-        //-stop current song, update playing div to new song, play new song
-        //-update isPlaying
-        else{
-            console.log('We are trying to play a different song')
-            //If a currSong is being played, stop it before playing new song
-            if (currSong){
-                console.log(`currSong is ${currSong}`)
-                stop(currSong)
-            }
-            changeCurrSongDiv(entireSongDiv)
-            console.log(`SongIDTarget is: ${songIDTarget}`)
-            updateCurrSong(songIDTarget)
-            play(songIDTarget)
-            updateIsPlaying()
-        }
-    }
-    else{
-        console.log('Not a proper song Div ID')
-    }  
-})
 
 
 
-//BOT APP AUDIO HANDLER
+                    //BOT APP AUDIO HANDLER
 
 
 
 playPause.addEventListener('click', function(evt){
+    console.log('playpause clicked')
+    
     console.log(evt)
     let songID = evt.path[4].dataset.song
     console.log(songID)
---
+++ static/js/library.js
@@ -0,0 +1,51 @@
+
+
+
+//MAIN LIBRARY AUDIO HANDLER
+
+
+
+songDiv.addEventListener("click", function(evt){
+
+    let entireSongDiv = evt.path[1]
+    let songDivID = evt.path[0].id
+    //this is the ID we want for document.getEle... to use audio. commands on
+    let songIDTarget = `audio_${songDivID}`
+    let isProperSongID = endsWithNumber(songIDTarget)
+    
+    //Did we target a proper songDiv?       
+    if (isProperSongID){
+        console.log('We clicked a proper song ID')
+        //Is it the previously targetted songDiv that is already playing or paused?
+        //-Then pause/play it and update isPlaying
+        if (songIDTarget == currSong){
+            console.log(`Our target: ${songIDTarget} is the same as current song: ${currSong}`)
+            if (isPlaying){
+                pause(songIDTarget)
+            }
+            else{
+                play(songIDTarget)
+            }
+            updateIsPlaying()
+        }   
+        //Is it another song we are trying to play?
+        //-stop current song, update playing div to new song, play new song
+        //-update isPlaying
+        else{
+            console.log('We are trying to play a different song')
+            //If a currSong is being played, stop it before playing new song
+            if (currSong){
+                console.log(`currSong is ${currSong}`)
+                stop(currSong)
+            }
+            changeCurrSongDiv(entireSongDiv)
+            console.log(`SongIDTarget is: ${songIDTarget}`)
+            updateCurrSong(songIDTarget)
+            play(songIDTarget)
+            updateIsPlaying()
+        }
+    }
+    else{
+        console.log('Not a proper song Div ID')
+    }  
+})
