c94233d101de0d816322c9d7d19c1d7b5a782295
= <jakelarsendk@gmail.com>
2021-10-10 08:51:57 -0500

    2-input audio logic updated (see audiologic)

--- static/js/audio2.js
+++ static/js/audio2.js
@@ -10,6 +10,7 @@ let nextBtn = document.getElementById('next-btn')
 let prevBtn = document.getElementById('prev-btn')
 let isPlaying = false
 let currSong = null
+let lastInput = 'neutral'
 
 
 
@@ -128,6 +129,7 @@ function audioHandler(song, prevSong="", nextSong=""){
             }
             //IS IT A DIFFERENT SONG
             else{
+                //PLAY IT
                 updateCurrSongDiv(song)
                 updatePrevSongDiv(prevSong)
                 updateNextSongDiv(nextSong)
@@ -142,12 +144,14 @@ function audioHandler(song, prevSong="", nextSong=""){
         else{
             //IS IT THE SAME SONG
             if(currSong.id == song.id){
+                //PLAY IT
                 play(currSong)
                     isPlaying = true
                     swapPlayPause()
             }
             //IS IT A DIFFERENT SONG
             else{
+                //PLAY IT
                 updateCurrSongDiv(song)
                 updatePrevSongDiv(prevSong)
                 updateNextSongDiv(nextSong)
@@ -177,68 +181,133 @@ function audioHandler(song, prevSong="", nextSong=""){
                     //BOT APP AUDIO EVENT HANDLERS
 
 
-//GO TO NEXT SONG              
+
+/**
+ * Handle NEXT input command received:
+ * 
+ * If there is a current song
+ *  If it is playing and the last input wasn't NEXT
+ *  - Go to next Song
+ *  elif it is playing and the last input WAS NEXT
+ *  - Go to next Playlist
+ *  elif song isn't playing
+ *  - Play!
+ */          
 async function nextEvent(){
+    console.log('NEXT EVENT')
     if(currSong){
-        let currSongIDX = parseInt(document.getElementById('next-playing-con').dataset.songidx)
-        let pll = parseInt(document.getElementById('next-playing-con').dataset.pll)
+        //PLAYING + NOTNEXT NEXT => NEXT SONG
+        if(isPlaying && lastInput != 'next'){
+            console.log('NEXT EVENT = NEXT SONG')
+            let currSongIDX = parseInt(document.getElementById('next-playing-con').dataset.songidx)
+            let pll = parseInt(document.getElementById('next-playing-con').dataset.pll)
+    
+            console.log(`Our New Song IDX is : ${currSongIDX}`)
+            console.log(`Our playlist length is : ${pll}`)
+    
+            let nextSongIDX = ""
+            if (currSongIDX == pll){
+                nextSongIDX = 1
+            }
+            else{
+                nextSongIDX = currSongIDX + 1
+            }
+    
+            let prevSongIDX = ""
+            if (currSongIDX == 1){
+                prevSongIDX = pll
+            }
+            else{
+                prevSongIDX = currSongIDX - 1
+            }
+            // console.log(`pll: ${pll}, prevSongIDX: ${prevSongIDX}, currSongIDX: ${currSongIDX}, nextSongIDX: ${nextSongIDX}`)
+            let song = await getSongData(currSongIDX)
+            let prevSong = await getSongData(prevSongIDX)
+            let nextSong = await getSongData(nextSongIDX)
+    
+            lastInput = 'next'
 
-        console.log(`Our New Song IDX is : ${currSongIDX}`)
-        console.log(`Our playlist length is : ${pll}`)
-
-        let nextSongIDX = ""
-        if (currSongIDX == pll){
-            nextSongIDX = 1
+            //SEND DATA FOR UPDATES TO OUR MAIN AUDIO HANDLER
+            audioHandler(song, prevSong, nextSong)
         }
-        else{
-            nextSongIDX = currSongIDX + 1
+        //PLAYING + NEXT NEXT => NEXT PLAYLIST
+        else if(isPlaying && lastInput == 'next'){
+            //CYCLE PLAYLIST LOGIC
+            console.log('NEXT EVENT = NEXT PLAYLIST')
+            //Using next playlist here to reset logic for next
+            lastInput = 'next playlist'
         }
-
-        let prevSongIDX = ""
-        if (currSongIDX == 1){
-            prevSongIDX = pll
+        //NOT PLAYING + NEXT => PLAY
+        else if(!isPlaying){
+            console.log('NEXT EVENT = PLAY')
+            lastInput = 'next'
+            playPauseEvent()
         }
-        else{
-            prevSongIDX = currSongIDX - 1
-        }
-        // console.log(`pll: ${pll}, prevSongIDX: ${prevSongIDX}, currSongIDX: ${currSongIDX}, nextSongIDX: ${nextSongIDX}`)
-        let song = await getSongData(currSongIDX)
-        let prevSong = await getSongData(prevSongIDX)
-        let nextSong = await getSongData(nextSongIDX)
-
-        //SEND DATA FOR UPDATES TO OUR MAIN AUDIO HANDLER
-        audioHandler(song, prevSong, nextSong)
     }
 }
 
-//GO TO PREV SONG
+/**
+ * Handle PREVIOUS input command received:
+ * 
+ * If there is a current song
+ *  If it is playing
+ *  - PAUSE
+ *  elif it is not playing and last event WAS PREV
+ *  - Go to prev song
+ *  elif it is not playing and last event WAS NEUTRAL
+ *  - Restart song
+ */   
 async function prevEvent(){
+    console.log('PREV EVENT')
     if(currSong){
-        let currSongIDX = parseInt(document.getElementById('prev-playing-con').dataset.songidx)
-        let pll = parseInt(document.getElementById('prev-playing-con').dataset.pll)
+        //PLAYING + PREV => PAUSE
+        if(isPlaying){
+            console.log('PREV EVENT = PAUSE')
+            lastInput = 'prev'
+            playPauseEvent()
+        }
+        //PREV PREV => PREV SONG
+        else if (!isPlaying && lastInput == 'prev'){
+            console.log('PREV EVENT = PREV SONG')
+            let currSongIDX = parseInt(document.getElementById('prev-playing-con').dataset.songidx)
+            let pll = parseInt(document.getElementById('prev-playing-con').dataset.pll)
 
-        let prevSongIDX = ""
-        if (currSongIDX == 1){
-            prevSongIDX = pll
-        }
-        else{
-            prevSongIDX = currSongIDX - 1
-        }
+            let prevSongIDX = ""
+            if (currSongIDX == 1){
+                prevSongIDX = pll
+            }
+            else{
+                prevSongIDX = currSongIDX - 1
+            }
+            let nextSongIDX = ""
+            if (currSongIDX == pll){
+                nextSongIDX = 1
+            }
+            else{
+                nextSongIDX = currSongIDX + 1
+            }
+            // console.log(`pll: ${pll}, prevSongIDX: ${prevSongIDX}, currSongIDX: ${currSongIDX}, nextSongIDX: ${nextSongIDX}`)
+            let song = await getSongData(currSongIDX)
+            let prevSong = await getSongData(prevSongIDX)
+            let nextSong = await getSongData(nextSongIDX)
 
-        let nextSongIDX = ""
-        if (currSongIDX == pll){
-            nextSongIDX = 1
-        }
-        else{
-            nextSongIDX = currSongIDX + 1
-        }
-        // console.log(`pll: ${pll}, prevSongIDX: ${prevSongIDX}, currSongIDX: ${currSongIDX}, nextSongIDX: ${nextSongIDX}`)
-        let song = await getSongData(currSongIDX)
-        let prevSong = await getSongData(prevSongIDX)
-        let nextSong = await getSongData(nextSongIDX)
+            lastInput = 'prev'
 
-        audioHandler(song, prevSong, nextSong)
+            audioHandler(song, prevSong, nextSong)
+        }
+        //PREV NEUTRAL PREV = RESTART SONG
+        else if (!isPlaying && lastInput != 'prev'){
+            console.log('PREV EVENT = PREV SONG')
+            lastInput = 'prev'
+            //RESTART SONG LOGIC HERE
+        }  
     }
+}
+
+//ON NEUTRAL EVENT - UPDATE LASTINPUT
+function neutralEvent(){
+    lastInput = 'neutral'
+    console.log("LAST INPUT:", lastInput)
 }
 
 //PLAY OR PAUSE
