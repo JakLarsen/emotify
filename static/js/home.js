b9b4ed7cc369ab5121976265b0222b33cfa5ebee
= <jakelarsendk@gmail.com>
2021-10-10 07:56:35 -0500

    App populated; Liked Songs connected from home, sidebar.

--- static/js/home.js
+++ static/js/home.js
@@ -1,6 +1,15 @@
+//YOUR PLAYLISTS PAGE FROM HOME
 $('.home-your-playlist-playlist').click(function(evt){
     console.log(evt)
     let target = evt.currentTarget.id
+    console.log(target)
     let playlistID = target.substr(14)
+    midCon.load(`/playlist/${playlistID}`)
+});
+//YOUR LIKED MUSIC FROM HOME PAGE
+$('#home-favorites-con').click(async function(evt){
+    console.log('home-favorties-con clicked')
+    let playlistData = await axios.get('/users/current/liked-songs-id')
+    let playlistID = parseInt(playlistData.data)
     midCon.load(`/playlist/${playlistID}`)
 });
